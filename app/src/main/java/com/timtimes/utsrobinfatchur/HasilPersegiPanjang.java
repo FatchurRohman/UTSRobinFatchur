fb42b5c10c754781bd60b0b0ead9c6e551a8b1b6
FatchurRohman <fatchurrohman024@gmail.com>
2017-11-22 16:02:59 +0700

    Initial commit

---
+++ app/src/main/java/com/timtimes/utsrobinfatchur/HasilPersegiPanjang.java
@@ -0,0 +1,22 @@
+package com.timtimes.utsrobinfatchur;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class HasilPersegiPanjang extends AppCompatActivity {
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_persegi_panjang);
+        getSupportActionBar().setTitle("UTS ROBIN DAN FATUR");
+
+        tv = (TextView)findViewById(R.id.tv);
+        Intent i = getIntent();
+        String nilai = i.getStringExtra("nilai");
+        tv.setText(nilai);
+    }
+}
---
+++ app/src/main/java/com/timtimes/utsrobinfatchur/HasilSegitiga.java
@@ -0,0 +1,21 @@
+package com.timtimes.utsrobinfatchur;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class HasilSegitiga extends AppCompatActivity {
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_segitiga);
+        getSupportActionBar().setTitle("UTS ROBIN DAN FATUR");
+
+        tv = (TextView)findViewById(R.id.tv);
+        Intent i = getIntent();
+        String nilai = i.getStringExtra("nilai");
+        tv.setText(nilai);
+    }
+}
